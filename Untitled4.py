b5914c7e929fa711740d4620372c3aa9211abfba
Yernat02 <79129976+Yernat02@users.noreply.github.com>
2021-03-16 14:58:19 +0600

    Add files via upload

---
+++ Untitled4.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+qty0ranges=input(0)
+price0range=input()
+
+qty0ranges=int(qty0ranges)
+price0range=float(price0range)
+
+sum0ranges=qty0ranges*price0range
+
+print("Заплатите", sum0ranges, "руб")
+
